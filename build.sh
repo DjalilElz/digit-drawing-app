7a576d5dbc5a05559ea27041ec54f6285291b022
DjalilElz <abdeldjalil.elazizi@ensia.edu.com>
2025-12-05 22:06:00 +0100

    Use direct database connection for migrations

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-# Remove pgbouncer parameter for migrations (not supported)
-export MIGRATION_DB_URL="${DATABASE_URL/\?pgbouncer=true/}"
-DATABASE_URL="$MIGRATION_DB_URL" python manage.py migrate
+# Use direct connection for migrations (port 5432 instead of pooler)
+# Replace pooler URL with direct connection
+DIRECT_URL="${DATABASE_URL//6543/5432}"
+DIRECT_URL="${DIRECT_URL//\?pgbouncer=true/}"
+echo "Running migrations..."
+DATABASE_URL="$DIRECT_URL" python manage.py migrate --noinput
